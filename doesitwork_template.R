6ace71bf76d6349defeb733a95c0769f0e26c01a
Melle Starke <mellestarke@gmail.com>
2017-10-06 15:48:02 +0200

    initial commit

---
+++ doesitwork_template.R
@@ -0,0 +1,204 @@
+# doesitwork.R
+
+#setwd("C:/Users/u341138/Dropbox/Documents/Teaching/2017-2018/Bayesian statistics/R/DBDA2Eprograms")
+
+# Optional generic preliminaries:
+graphics.off() # This closes all of R's graphics windows.
+rm(list=ls())  # Careful! This clears all of R's memory!
+
+# Required packages for this exercise.
+require(rjags)
+require(coda)
+require(BEST)
+source("DBDA2E-utilities.R")
+
+
+plotGaussian <- function(data, mean, sd) {
+  plot.new()
+  par(mfrow=c(1,1))
+  h = hist(data, breaks = 50)
+  xrange = seq(min(data),max(data),length=50) 
+  gaussian_distribution = dnorm(xrange, mean=mean, sd = sd)
+  scaled_gaussian = gaussian_distribution * diff(h $ mids[1:2])*length(data) 
+  lines(xrange, scaled_gaussian)
+}
+
+# This code provides an outline; additional code needs to be added by yourself!
+
+
+
+#----------   Model 1: learning a mean   --------------
+
+# Set the initial parameters
+
+sigma = 2.0
+N = 1000
+mu = 5
+
+# Generate observations here
+x = rep(0, N)
+
+# fill vector x here
+
+for(i in 1:N){
+  mean <- rnorm(1,mu,sigma)
+  new_x <- rnorm(1,mean,sigma)
+  x[i] <- new_x
+}
+
+hist(x,breaks = 50)
+
+
+#define starting mean
+mu_hat <- 1
+sigma_hat <- 1
+# Write down the same model in JAGS notation.
+model1.string = "
+  model {
+    # Prior
+  
+      mu ~ dnorm(mu_hat, 1/sigma_hat)
+  
+
+    # Likelihood
+      for(i in 1:N){
+        x[i] ~ dnorm(mu, 1/sigma_hat)
+      }
+  }
+"
+
+# JAGS usually reads models from a text file; here we use a string as a fake file.   
+model1.spec = textConnection(model1.string)
+
+# Construct the object containing both the model specification as well as the data and some sampling parameters.
+jagsmodel1 <- jags.model(model1.spec,
+                         data = list('N' = N,                    # the number of data points                       
+                                     'mu_hat' = mu_hat,          # prior value
+                                     'sigma_hat' = sigma_hat,            # the standard deviation of the likelihood
+                                     'x' = x                     # the observations
+                                     ),  
+                         n.chains=4)
+
+mcmciterations = 10000 
+samples = coda.samples(jagsmodel1,
+                       c('mu'), # which variables do you want to monitor?
+                       n.iter=mcmciterations)
+
+# plot/interpret the results
+
+mcmcsummary = summary(samples)
+mcmcsummary $ statistics
+
+plot1 <- plotPost(samples, xlab = "mu", ylab = "P(mu)")
+
+
+#----------   Model 2: learning mean and variance   --------------
+
+# Set the initial parameters
+sigma = 2.0
+N = 10
+mu = 10
+a = 1
+b = 1
+
+sigma_hat = 1
+mu_hat = 1
+
+# Generate observations here
+x = rep(0, N)
+
+for(i in 1:N){
+  sd <- rgamma(a,b)
+  mean <- rnorm(1,mu,sigma)
+  new_x <- rnorm(1,mean,sd)
+  x[i] <- new_x
+}
+
+hist(x, breaks = 50)
+
+
+# Write down the same model in JAGS notation.
+model1.string = "
+  model {
+    # Prior
+    mu ~ dnorm(mu_hat, 1/sigma_hat)
+    sigma ~ dgamma(a,b)
+
+    # Likelihood
+    for(i in 1:N){
+        x[i] ~ dnorm(mu, 1/sigma)
+}
+    
+  }
+"
+
+# JAGS usually reads models from a text file; here we use a string as a fake file.   
+model1.spec = textConnection(model1.string)
+
+# Construct the object containing both the model specification as well as the data and some sampling parameters.
+jagsmodel2 <- jags.model(model1.spec,
+                         data = list('N' = N,                    # the number of data points
+                                     'x' = x,                   # the observations
+                                      'sigma_hat' = sigma_hat,
+                                     'mu_hat' = mu_hat,
+                                     'a' = a,
+                                     'b' = b
+                                     ),  
+                         n.chains=4)
+
+mcmciterations = 10000 
+samples2 = coda.samples(jagsmodel2,
+                       c('mu','sigma'), # which parameters do you want to monitor?
+                       n.iter=mcmciterations)
+
+
+
+# plot/interpret the results
+mcmcsummary2 = summary(samples2)
+mcmcsummary2 $ statistics
+
+plot2_mu <- plotPost(samples2[,1], xlab = "mu", ylab = "P(mu)")
+plot2_sigma <- plotPost(samples2[,2], xlab = "sigma", ylab = "P(sigma)")
+
+
+#----------   Model 3: the wrong model   --------------
+
+
+# Set the initial parameters
+N = 1000
+df = 1
+
+# Generate observations here
+x = rep(0, N)
+
+
+hist(x, breaks = 50)
+
+
+# Write down the same model in JAGS notation.
+model3.string = "
+model {
+  # Prior
+  
+  
+  # Likelihood
+}
+"
+
+# JAGS usually reads models from a text file; here we use a string as a fake file.   
+model3.spec = textConnection(model3.string)
+
+# Construct the object containing both the model specification as well as the data and some sampling parameters.
+jagsmodel3 <- jags.model(model3.spec,
+                         data = list('N' = N,                    # the number of data points
+                                     'x' = x                     # the observations
+                         ),  
+                         n.chains=4)
+
+mcmciterations = 10000 
+samples = coda.samples(jagsmodel3,
+                       c(''), # which variables do you want to monitor?
+                       n.iter=mcmciterations)
+
+
+# plot/interpret the results
